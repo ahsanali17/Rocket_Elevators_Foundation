261ef02ffc083ba690f5aa6d928694e6302a0159
ACLTearr <burroughskaelen@gmail.com>
2021-03-09 11:45:43 -0500

    Added status change check to Twilio

--- app/models/elevator.rb
+++ app/models/elevator.rb
@@ -4,17 +4,24 @@ class Elevator < ApplicationRecord
 
     #if the elevator status change to Intervention, you'll receive a text message
     def twilio_txt
-        if self.status == "Intervention" or self.status == "intervention" then
-            account_sid = ENV['TWILIO_ACCOUNT_SID']
-            auth_token = ENV['TWILIO_AUTH_TOKEN']
-            #phone_to = self.column.battery.building.technical_contact_phone_for_the_building
-            @client = Twilio::REST::Client.new(account_sid, auth_token)    
-            
-            message = @client.messages.create(
-                body: "Mr/Mrs #{self.column.battery.building.full_name_of_the_technical_contact_for_the_building}. The Elevator ID: #{self.id}, with Serial Number: #{self.serial_number} require maintenance, your status has been changed to: #{self.status}",
-                from: '+16106869129',
-                to: '+14184469990'
-            )            
+        notify = self.status_changed?
+        if notify 
+            if self.status == "Intervention" or self.status == "intervention" then
+                account_sid = ENV['TWILIO_ACCOUNT_SID']
+                auth_token = ENV['TWILIO_AUTH_TOKEN']
+                #phone_to = self.column.battery.building.technical_contact_phone_for_the_building
+                @client = Twilio::REST::Client.new(account_sid, auth_token)    
+                
+                message = @client.messages.create(
+                    body: "Hello Mr/Mrs #{self.column.battery.building.full_name_of_the_technical_contact_for_the_building}. The Elevator ID: #{self.id}, with Serial Number: #{self.serial_number} requires maintenance, the elevators status has been changed to: #{self.status}",
+                    # Cristiane numbers:
+                    # from: '+16106869129',
+                    # to: '+14184469990'
+                    # Kaelen numbers:
+                    from: '+12248582267',
+                    to: '+13062407737'
+                ) 
+            end           
         end        
     end
 
