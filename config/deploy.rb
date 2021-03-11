432cd74da1edd1273e88d6801f65d2a180b21744
ACLTearr <burroughskaelen@gmail.com>
2021-03-11 14:35:51 -0500

    Added branch path

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,6 +5,7 @@ before 'deploy', 'rvm1:install:ruby'
 set :rvm_map_bins, [ 'rake', 'gem', 'bundle', 'ruby', 'puma', 'pumactl' ]
 set :application, "KaelenBurroughs"
 set :repo_url, "https://github.com/ACLTearr/Rocket_Elevators_API"
+set :branch, "main"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
