435fa7187c7143d0f708697ab55e42dd93a5a1a6
Cristiane Santiago <cris.s.santiago@gmail.com>
2021-03-11 18:23:35 -0500

    Tab Watson

---
+++ lib/rails_admin/watson.rb
@@ -0,0 +1,29 @@
+module RailsAdmin
+    module Config
+      module Actions
+        class Watson < RailsAdmin::Config::Actions::Base
+          RailsAdmin::Config::Actions.register(self)
+  
+          register_instance_option :root? do
+            true
+          end
+  
+          register_instance_option :breadcrumb_parent do
+            nil
+          end
+  
+          register_instance_option :auditing_versions_limit do
+            100
+          end
+          
+          register_instance_option :link_icon do
+            'icon-music'
+          end
+  
+          register_instance_option :statistics? do
+            true
+          end
+        end
+      end
+    end
+  end
