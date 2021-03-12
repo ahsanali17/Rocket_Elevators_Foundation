fb75bf81b0b1f54678f99dccfcc8845c24426107
Cristiane Santiago <cris.s.santiago@gmail.com>
2021-03-12 11:12:48 -0500

    Watson with audio in the Admin, but walks without reading the controller.

--- app/controllers/watson_controller.rb
+++ app/controllers/watson_controller.rb
@@ -1,35 +1,36 @@
-class WatsonController < ApplicationController
-    require "ibm_watson/authenticators"
-    require "ibm_watson/text_to_speech_v1"
-    include IBMWatson
+require "json"
+require "ibm_watson"
+require "ibm_watson/authenticators"
+require "ibm_watson/text_to_speech_v1"
+include IBMWatson
 
-    def welcome
-        authenticator = IBMWatson::Authenticators::IamAuthenticator.new(
+
+class WatsonController < ActionController::Base
+    skip_before_action :verify_authenticity_token
+
+  
+    def speak
+  
+        authenticator = Authenticators::IamAuthenticator.new(
             apikey: ENV["TEXT_TO_SPEECH_APIKEY"]
         )
         text_to_speech = TextToSpeechV1.new(
             authenticator: authenticator
         )
         text_to_speech.service_url = "https://api.us-south.text-to-speech.watson.cloud.ibm.com/instances/b0629bf8-7de7-4d25-831f-39083fe5af36"
+            
+        message = "Greetings user #{current_user.id}"
 
-        name = "#{current_employee.first_name} #{current_employee.last_name}"
-        total_elevators = Elevator.count
-        total_buildings = Building.count
-        total_customers = Customer.count
-        out_of_service = Elevator.where.not(status:'active').count
-        total_quotes = Quote.count
-        total_leads = Lead.count
-        total_batteries = Battery.count
-        total_cities = Address.distinct.count(:city)
+        response = text_to_speech.synthesize(
+            text: message,
+            accept: "audio/wav",
+            voice: "en-GB_KateV3Voice"
+        ).result
 
-        File.open("app/assets/audio/welcome.wav", "wb") do |audio|
-            response = text_to_speech.synthesize(
-                text: "Welcome, #{name}. You have #{total_elevators.to_s} elevators deployed in #{total_buildings.to_s} building belonging to #{total_customers.to_s} customers. Currently there are #{out_of_service.to_s} elevators out of service and #{total_quotes.to_s} quotes waiting to be processed. There are also #{total_leads.to_s} leads waiting and #{total_batteries.to_s} batteries running across #{total_cities.to_s} cities",
-                accept: "audio/wav",
-                voice: "en-GB_KateVoice"
-            )
-            audio.write(response.result)
-            redirect_back(fallback_location:"/")
-        end
+        File.open("#{Rails.root}/public/hello_world.wav", "wb") do |audio_file|
+                        audio_file.write(response)
+        end 
+           
     end
 end
+
